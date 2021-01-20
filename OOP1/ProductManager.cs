e48004121d6827c0418c82f8c60010d7e25bdd78
talhauysal <talhauysal@TALHA-UYSAL>
2021-01-21 00:09:34 +0300

    Yazılım Kampı Çalışmaları-2 21.01.2021

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName+" eklendi");
+
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi");
+        }
+        public void Delete(Product product)
+        {
+            Console.WriteLine(product.ProductName + " silindi");
+        }
+        /*void nedir ? geriye bir değer döndürmeyecek ise void kullanılabilir, 
+        int , string vb. gibi değişkenler türünden bir fonksiyon yazılacak ise return komutu 
+        kullanılabilir.*/
+
+
+
+    }
+}
